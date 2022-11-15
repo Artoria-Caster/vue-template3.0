a6c847148236a20da8f42334466db5f21dcb47f6
lzy <1115688034@qq.com>
2022-11-15 16:58:35 +0800

    添加一个http的示例

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+//请求的示例
+
+// import HttpApi from "@/utils/https";
+// import global from "@/utils/global";
+// import store from "@/store";
+
+// const http = new HttpApi({baseURL: global.api.API_ADDR})
+
+//一个登录的示例
+// //内网登录
+// export const userLogin = (params, success, failed) => {
+//     http.post({}, "/gisadmin-system/auth/login", params, (res) => {
+//         //内网登录成功
+//         if (res.code === 200) {
+//             success && success()
+//         } else {
+//             failed && failed()
+//         }
+//
+//     }, () => {
+//         // 如果内网登录不成功，执行本地登录
+//         failed && failed()
+//     })
+// }
+
+
+// export default http

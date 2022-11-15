cc626e57a89138134f439c7350f7c50866f9d232
lzy <1115688034@qq.com>
2022-11-15 16:55:49 +0800

    接入一个pinia演示

--- src/pages/default/home/Home.vue
+++ src/pages/default/home/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <h1>Home</h1>
+  <button @click=' counterStore.increment()'>按钮</button>
+  <div>数据：{{ counterStore.count }}</div>
 </template>
 
-<script>
-export default {
-  name: 'HomeIndex'
-}
+<script setup>
+import { useCounterStore } from '@/stores/home'
+const counterStore = useCounterStore()
 </script>
 
 <style scoped>

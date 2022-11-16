5ecc01a2b9e1c9a7d75bb46c9cf866622ea8600b
lzy <1115688034@qq.com>
2022-11-16 12:02:57 +0800

    调整vite设置   调整格式 添加技术选型文档

--- vite.config.js
+++ vite.config.js
@@ -24,12 +24,12 @@ export default defineConfig({
 	},
 	//开发配置  npm run dev
 	server: {
-		port: 3001,//端口号
-		strictPort: false,//是否是严格的端口号，如果true，端口号被占用的情况下，vite会退出
+		port: 3001, //端口号
+		strictPort: false, //是否是严格的端口号，如果true，端口号被占用的情况下，vite会退出
 		host: 'localhost',
-		cors: true,//为开发服务器配置 CORS , 默认启用并允许任何源
-		https: false,//是否支持http2 如果配置成true 会打开https://localhost:3001/xxx;
-		open: true,//是否自动打开浏览器
+		cors: true, //为开发服务器配置 CORS , 默认启用并允许任何源
+		https: false, //是否支持http2 如果配置成true 会打开https://localhost:3001/xxx;
+		open: true, //是否自动打开浏览器
 		// 反向代理 跨域配置
 		// proxy: {
 		// 	'/api': {
@@ -40,11 +40,7 @@ export default defineConfig({
 		// }
 	},
 	// 打包配置 npm run build
-	build:{
-		//指定输出路径
-		outDir: "dist",
-		//生成静态资源的存放路径
-		assetsDir: "assets",
+	build: {
 		//小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项
 		assetsInlineLimit: 4096,
 		//启用/禁用 CSS 代码拆分
@@ -53,19 +49,20 @@ export default defineConfig({
 		sourcemap: false,
 		//自定义底层的 Rollup 打包配置
 		rollupOptions: {
-			input:{//可以配置多个，表示多入口
-				index:path.resolve(__dirname,"index.html"),
+			input: {
+				//可以配置多个，表示多入口
+				index: path.resolve(__dirname, 'index.html'),
 				// project:resolve(__dirname,"project.html")
 			},
-			output:{
+			output: {
 				// chunkFileNames:'static/js/[name]-[hash].js',
 				// entryFileNames:"static/js/[name]-[hash].js",
 				// assetFileNames:"static/[ext]/name-[hash].[ext]"
-			}
+			},
 		},
 		//默认情况下，若 outDir 在 root 目录下，则 Vite 会在构建时清空该目录。
 		emptyOutDir: true,
 		//chunk 大小警告的限制
-		chunkSizeWarningLimit: 500
-	}
+		chunkSizeWarningLimit: 500,
+	},
 })

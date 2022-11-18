224573cd31e10ca47c82be067add1220c1edaece
lzy <1115688034@qq.com>
2022-11-18 10:36:03 +0800

    简化路由，设备探测不再适用于路由组装

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import global from '@/utils/global'
 
 import defaultRouter from '@/router/default/index'
 
-let routes = []
+//暂时仅仅使用default 设备探测仅保存状态，不再作为路由封装使用
+let routes = defaultRouter
 
-switch (global.runEnviron) {
-	case 'default':
-		routes = defaultRouter
-		break
-}
+// switch (global.runEnviron) {
+// 	case 'default':
+// 		routes = defaultRouter
+// 		break
+// }
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
